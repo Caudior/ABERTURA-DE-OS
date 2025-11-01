306ab7336a44f469deae415d2a907dc9e9e2e275
[dyad] <git@dyad.sh>
2025-10-31 21:50:16 -0300

    [dyad] Corrigindo erro React.Children.only movendo Toaster e Sonner para fora do TooltipProvider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,28 @@ import { AuthProvider } from "@/contexts/AuthContext";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import ServiceOrderPage from "./pages/ServiceOrderPage";
-import NewServiceOrderPage from "./pages/NewServiceOrderPage"; // Import the new NewServiceOrderPage
+import NewServiceOrderPage from "./pages/NewServiceOrderPage";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    {/* Toaster e Sonner movidos para fora do TooltipProvider */}
+    <Toaster />
+    <Sonner />
     <TooltipProvider>
-      {/* Envolvendo os múltiplos filhos em um React.Fragment */}
-      <>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <AuthProvider>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/service-orders" element={<ServiceOrderPage />} />
-              <Route path="/service-orders/new" element={<NewServiceOrderPage />} /> {/* New Route for creating service orders */}
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </AuthProvider>
-        </BrowserRouter>
-      </>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/service-orders" element={<ServiceOrderPage />} />
+            <Route path="/service-orders/new" element={<NewServiceOrderPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
