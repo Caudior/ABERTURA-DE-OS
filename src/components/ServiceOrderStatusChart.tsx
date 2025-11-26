a9d14bb60c74cbf114ddcb422ac6a9e129fbc1fd
[dyad] <git@dyad.sh>
2025-11-25 23:54:11 -0300

    [dyad] Ajustei a altura da legenda do gráfico de pizza para evitar que o texto seja cortado. - wrote 1 file(s)

--- src/components/ServiceOrderStatusChart.tsx
+++ src/components/ServiceOrderStatusChart.tsx
@@ -84,7 +84,7 @@ const ServiceOrderStatusChart: React.FC = () => {
                 ))}
               </Pie>
               <Tooltip />
-              <Legend verticalAlign="bottom" align="center" height={36} />
+              <Legend verticalAlign="bottom" align="center" height={70} /> {/* Aumentado a altura para 70 */}
             </PieChart>
           </ResponsiveContainer>
         ) : (
